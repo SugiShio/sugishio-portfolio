b05c9c40a199a4d5253acfc750f9d8487799ddfb
Shoko Sugito <fruitmachine0@gmail.com>
2021-09-06 21:38:47 +0900

    chore: ESLint設定

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,19 +2,22 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    node: true
+    node: true,
   },
   parserOptions: {
     parser: '@babel/eslint-parser',
-    requireConfigFile: false
+    requireConfigFile: false,
   },
-  extends: [
-    '@nuxtjs',
-    'plugin:nuxt/recommended',
-    'prettier'
-  ],
-  plugins: [
-  ],
-  // add your custom rules here
-  rules: {}
+  extends: ['@nuxtjs', 'plugin:nuxt/recommended', 'prettier'],
+  plugins: ['prettier'],
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+    'no-unused-vars': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+    'no-unreachable': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+    'no-useless-return':
+      process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+    'vue/order-in-components':
+      process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+  },
 }
