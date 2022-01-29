c61fd0b6e57b9e204238274093bd688be9e0608e
Shoko Sugito <fruitmachine0@gmail.com>
2022-01-29 09:40:38 +0900

    feat: プロフィールにアカウント追加

--- components/molecules/profileCard.vue
+++ components/molecules/profileCard.vue
@@ -1,8 +1,13 @@
 <template lang="pug">
-  section.s-profile-card
-    nuxt-link(:to='{ name: "profile"}')
+section.s-profile-card
+  .s-profile-card__link
+    nuxt-link(:to='{ name: "profile" }')
       .s-profile-card__name {{ profile.displayName }}
       .s-profile-card__copy {{ profile.copy }}
+  ul.s-profile-card__accounts
+    li.s-profile-card__account(v-for='account in profile.accounts')
+      a(:href='account.url', target='_blank')
+        img(:src='account.imagePath')
 </template>
 
 <script>
@@ -15,12 +20,32 @@ export default {
 </script>
 <style lang="scss">
 .s-profile-card {
-  transition: opacity 0.3s;
-  &:hover {
-    opacity: 0.6;
+  &__link {
+    transition: opacity 0.3s;
+    &:hover {
+      opacity: 0.6;
+    }
   }
   &__copy {
     font-size: 13px;
+  }
+  &__accounts {
+    margin: 20px 0;
+  }
+  &__account {
+    display: inline-block;
+    width: 24px;
+    filter: saturate(0);
+    opacity: 0.7;
+    transition: filter 0.3s, opacity 0.3s;
+
+    &:hover {
+      opacity: 1;
+      filter: saturate(100%);
+    }
+    & + & {
+      margin-left: 10px;
+    }
   }
 }
 </style>
