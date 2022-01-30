f8b34bcc893c7eaab832c4492c022fbfd8e3fd46
Shoko Sugito <fruitmachine0@gmail.com>
2022-01-30 13:54:54 +0900

    feat: フッター

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,11 @@
 <template lang="pug">
 .s-l-default
-  header.s-l-default__header
   main.s-l-default__main
     nuxt
+  footer.s-l-default__footer
+    .s-l-default__footer-title
+      nuxt-link(:to='{ name: "index" }') SugiShio Portforio
+    span.s-l-default__copyright © SugiShio
 </template>
 
 <script>
@@ -11,13 +14,13 @@ export default {
   computed: {
     deviceType() {
       return this.$store.state.deviceType
-    }
+    },
   },
   methods: {
     backPage() {
       this.$router.go(-1)
-    }
-  }
+    },
+  },
 }
 </script>
 
@@ -29,6 +32,30 @@ export default {
     @include screen-pc {
       padding: 80px;
     }
+  }
+
+  &__footer {
+    padding: 20px 40px;
+
+    @include screen-pc {
+      display: flex;
+      justify-content: space-between;
+      padding: 20px 80px;
+    }
+  }
+
+  &__footer-title {
+    transition: opacity 0.3s;
+    cursor: pointer;
+
+    &:hover {
+      opacity: 0.6;
+    }
+  }
+
+  &__copyright {
+    margin-top: 10px;
+    font-size: 13px;
   }
 }
 </style>
